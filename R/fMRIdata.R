355cc8c2e05782a30af704afc92d3103e91519d6
angeella <a.andreella8@gmail.com>
2020-10-01 17:38:59 +0200

    first commit

---
+++ R/fMRIdata.R
@@ -0,0 +1,14 @@
+#' functional Magnetic Resonance Imaging (fMRI) Data Package
+#'
+#' This package is meant to serve as a data collection tool for fMRI data.
+#'
+#'
+#' @author
+#' Angela Andreella \email{angela.andreella@phd.unipd.it},
+#'
+#' @docType package
+#' @name fMRIdata
+#' @exportPattern ^[[:alpha:]]+
+#' @examples
+#' # Please see the datasets
+NULL
